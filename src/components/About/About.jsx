37a67b3515aebc30b5ec7900d65c7adcd0945dbf
heba sabri <heba.sabri.183@gmail.com>
2025-02-27 18:12:59 +0200

    add custom hook

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,7 +1,16 @@
 import React from 'react'
+import useWindowSize from '../../hooks/useWindowSize';
 
 export default function About() {
+  const { width, height } = useWindowSize(); 
+
   return (
-    <div>About</div>
+    <div>
+      <h1>Window Size</h1>
+      <p>Width: {width}px</p>
+      <p>Height: {height}px</p>
+    </div>
   )
 }
+
+
